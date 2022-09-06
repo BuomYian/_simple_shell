a9c42c12f1c1fe89877809f4bca58fbc3c46324f
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-09-06 05:54:07 -0700

    Added betty file

---
+++ betty.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main - function to return sum of two numbers
+  *
+  *Return: Always 0.
+  */
+int main(void)
+{
+	int a;
+	int b;
+	int sum;
+
+	a = 5;
+	b = 10;
+	sum = a + b;
+	printf("The sum of two numbers is %d\n", sum);
+
+	return (0);
+}
